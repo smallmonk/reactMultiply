0f7056cfc7c1ebda6471841055962d2e1aa4a026
smallmonk <phuasj@gmail.com>
2021-01-23 20:32:16 +0800

    MATH-1: Apply bigger font; Remove redundant implementation

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import {Helmet} from "react-helmet";
 import './index.css';
 
-function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-}
-
 function TapButton(props) {
   return (
     <button className="tapbutton" onClick={props.onClick}>
@@ -41,8 +33,6 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
       firstNum: 5,
       secondNums: Array(12).fill(null),
       resultCheck: Array(12).fill(null),
@@ -51,18 +41,6 @@ class Board extends React.Component {
     for (i = 0; i < 12; i++) {
       this.state.secondNums[i] = (i+1);
     }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
   }
 
   handlePrevNum() {
@@ -177,30 +155,8 @@ class Board extends React.Component {
     }
   }
 
-  renderSquare(i) {
-    return (
-        <Square 
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)}
-        />
-    );
-  }
-
   render() {
-    // const winner = calculateWinner(this.state.squares);
-    // let status;
-    // if (winner) {
-    //   status = 'Winner: ' + winner;
-    // } else {
-    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    // }
-
     var grid = []
-    grid.push(
-      <div className="board-row">
-        {this.renderText("--------------------------------------------------------------------------")}
-      </div>
-    );
     
     var rowno;
     for (rowno = 0; rowno < 12; rowno++) {
@@ -214,13 +170,7 @@ class Board extends React.Component {
           {this.renderResultStatus(rowno)}
         </div>
       );
-      grid.push(
-        <div className="board-row">
-          {this.renderText("--------------------------------------------------------------------------")}
-        </div>
-      );
     }
-
 
     return (
       <div>
@@ -255,26 +205,6 @@ class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 function verifyResult(first, second, result) {
